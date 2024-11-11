82e5afd1e25fce96f57435ff26e2fdee9ef7cb69
HA_GO\antic <nmckinzie@smcm.edu>
2024-11-11 15:50:56 -0600

    created array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdowngenerateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'what is the title of your readme?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'give a description of your readme'
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'write installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'write usage information'
+    },
+    {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'add contribution guidelines'
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'give test instructions'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
